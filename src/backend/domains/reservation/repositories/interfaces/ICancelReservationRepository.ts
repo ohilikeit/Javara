2d7d2dfd1130dc5a2dd5fb2a40c040ad8586c454
gpwltl <gpwltl97@naver.com>
2024-11-12 00:54:57 +0900

    에러 임시 해결

--- src/backend/domains/reservation/repositories/interfaces/ICancelReservationRepository.ts
+++ src/backend/domains/reservation/repositories/interfaces/ICancelReservationRepository.ts
@@ -2,5 +2,5 @@ import { ReservationEntity } from '../../entity/ReservationEntity';
 
 export interface ICancelReservationRepository {
     cancelReservation(id: number): Promise<void>;
-    findById(id: number): Promise<ReservationEntity>;
+    //findById(id: number): Promise<ReservationEntity>;
 } 
